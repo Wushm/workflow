c575f47a2a35c5720223c3e52f8de8d338bc44c6
Wushm <wushm@hotmail.com>
2015-07-07 14:02:16 +0800

    add release number 0

--- workflow/ReleaseTool/form.py
+++ workflow/ReleaseTool/form.py
@@ -4,6 +4,7 @@ from django.forms import ModelForm, Textarea,Select
 
 version_number1_choices=(
     ('','-'),
+	('0','0'),
     ('1','1'),
     ('2','2'),
     ('3','3'),
@@ -13,6 +14,7 @@ version_number1_choices=(
 version_number2_choices=(
     ('','-'),
     ('1','1'),
+	('0','0'),
     ('2','2'),
     ('3','3'),
     ('4','4'),
@@ -21,6 +23,7 @@ version_number2_choices=(
 
 version_number3_choices=(
     ('','-'),
+	('0','0'),
     ('1','1'),
     ('2','2'),
     ('3','3'),
@@ -40,6 +43,7 @@ version_number3_choices=(
 
 version_number4_choices=(
     ('','-'),
+	('0','0'),
     ('1','1'),
     ('2','2'),
     ('3','3'),
